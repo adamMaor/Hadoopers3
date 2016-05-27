d6c40907f928176859009e562d8ebf87eceda5de
vadim khakham <vadim.khakham@gmail.com>
2016-05-27 16:19:47 +0300

    main change

--- final-project/src/main/java/univ/bigdata/course/MainRunner.java
+++ final-project/src/main/java/univ/bigdata/course/MainRunner.java
@@ -1,11 +1,16 @@
 package univ.bigdata.course;
 
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+
+
 public class MainRunner {
 
     public static void main(String[] args) {
-        /*
-        TODO: You need to add here all logic required to parse and execute commands.
-         */
-        System.out.println("Hello World.");
+        SparkConf conf = new SparkConf().setAppName("hw3").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> distFile = sc.textFile("C:\\GitHub\\Hadoopers3\\movies-simple.txt");
+        System.out.println(distFile.count());
     }
 }
