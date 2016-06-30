896e3a5690b994844d7430e4724034aabb94eb20
adamMaor <maorcpa.adam@gmail.com>
2016-06-30 18:09:32 +0300

    documentation for queries

--- final-project/src/main/java/univ/bigdata/course/movie/UserRecommendations.java
+++ final-project/src/main/java/univ/bigdata/course/movie/UserRecommendations.java
@@ -1,3 +1,11 @@
+/**
+ * Submitters information - Hadoopers team:
+ * Vadim Khakham 	vadim.khakham@gmail.com	311890156
+ * Michel Guralnik mikijoy@gmail.com 	306555822
+ * Gilad Eini 	giladeini@gmail.com	034744920
+ * Adam Maor 	maorcpa.adam@gmail.com	036930501
+ */
+
 package univ.bigdata.course.movie;
 
 import scala.Serializable;
